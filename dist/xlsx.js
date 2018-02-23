379d39a15bd9ed0b01ed52562a04c58a163c0862
DESKTOP-GEIKBQ7\Soghband <soghband@gmail.com>
2018-02-23 17:16:46 +0700

    fix buffer1

--- dist/xlsx.js
+++ dist/xlsx.js
@@ -1297,20 +1297,7 @@ function cc2str(arr) {
 	return o;
 }
 
-function getdata(data) {
-	if(!data) return null;
-	if(data.name.substr(-4) === ".bin") {
-		if(data.data) return char_codes(data.data);
-		if(data.asNodeBuffer && has_buf) return data.asNodeBuffer();
-		if(data._data && data._data.getContent) return Array.prototype.slice.call(data._data.getContent());
-	} else {
-		if(data.data) return data.name.substr(-4) !== ".bin" ? debom_xml(data.data) : char_codes(data.data);
-		if(data.asNodeBuffer && has_buf) return debom_xml(data.asNodeBuffer().toString('binary'));
-		if(data.asBinary) return debom_xml(data.asBinary());
-		if(data._data && data._data.getContent) return debom_xml(cc2str(Array.prototype.slice.call(data._data.getContent(),0)));
-	}
-	return null;
-}
+function getdata(data) { return (data && data.name.slice(-4) === ".bin") ? getdatabin(data) : getdatastr(data); }
 
 function safegetzipfile(zip, file) {
 	var f = file; if(zip.files[f]) return zip.files[f];
